47901da88f43a302ee8e68c419d6635bb28956c7
Ignacio Cabral <ignaciocabral97@gmail.com>
2024-08-06 02:58:00 -0300

    db connection

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/IgnacioAgustinCabral/notes-api/pkg/db"
 	"net/http"
 )
 
 func main() {
+	db.Init()
+	defer db.Close()
+
 	mux := http.NewServeMux()
 
 	err := http.ListenAndServe(":8080", mux)
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,1 +1,32 @@
 package db
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+var Conn *pgx.Conn
+
+func Init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	connString := os.Getenv("DATABASE_URL")
+	if connString == "" {
+		log.Fatalf("DATABASE_URL is required")
+	}
+
+	_, err = pgx.Connect(context.Background(), connString)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+}
+
+func Close() {
+	_ = Conn.Close(context.Background())
+}
