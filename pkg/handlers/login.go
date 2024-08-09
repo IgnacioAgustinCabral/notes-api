0afabe02aa3152b137aace0e67c790cb6e291bda
Ignacio Cabral <ignaciocabral97@gmail.com>
2024-08-09 02:42:13 -0300

    login returns jwt

--- pkg/handlers/login.go
+++ pkg/handlers/login.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"github.com/IgnacioAgustinCabral/notes-api/pkg/auth"
 	"github.com/IgnacioAgustinCabral/notes-api/pkg/db"
 	"github.com/IgnacioAgustinCabral/notes-api/pkg/payloads"
 	"github.com/jackc/pgx/v5"
@@ -23,7 +24,6 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	var hashedPass string
-	//query a db para saber si esta bien username y password
 	err = db.Conn.QueryRow(
 		context.Background(),
 		`SELECT u.password FROM "user".user u WHERE u.username = ($1)`,
@@ -43,5 +43,14 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode("Login successful")
+	jwt, err := auth.GenerateJWT(request.Username)
+	if err != nil {
+		http.Error(w, "Error generating JWT", http.StatusInternalServerError)
+		return
+	}
+	response := map[string]string{
+		"msg": "Login successful",
+		"jwt": jwt,
+	}
+	json.NewEncoder(w).Encode(response)
 }
